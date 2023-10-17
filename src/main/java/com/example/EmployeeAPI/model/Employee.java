b30f355bf4831c8c85906de9fb9262637cc04a86
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-17 09:43:41 +0300

    First Commit

--- src/main/java/com/example/EmployeeAPI/model/Employee.java
+++ src/main/java/com/example/EmployeeAPI/model/Employee.java
@@ -1,9 +1,15 @@
 package com.example.EmployeeAPI.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
