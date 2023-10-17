27816e98962c5dbf502e176a6bcf04bfcf853e45
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-17 10:06:30 +0300

    First Commit

---
+++ src/main/java/com/example/EmployeeAPI/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.example.EmployeeAPI.service;
+
+public interface EmployeeService {
+}
---
+++ src/main/java/com/example/EmployeeAPI/service/EmployeeServiceImp.java
@@ -0,0 +1,7 @@
+package com.example.EmployeeAPI.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImp implements EmployeeService{
+}
