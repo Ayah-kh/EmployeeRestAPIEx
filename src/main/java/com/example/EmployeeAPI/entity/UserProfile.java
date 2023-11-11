ebda5beeb4c7ff8ddbfdf2e4edab7db252a3c100
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-11-11 18:34:34 +0200

    First Commit

--- src/main/java/com/example/EmployeeAPI/entity/UserProfile.java
+++ src/main/java/com/example/EmployeeAPI/entity/UserProfile.java
@@ -1,11 +1,32 @@
 package com.example.EmployeeAPI.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
+@Entity
+@Table(name = "user_profiles")
 public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String phoneNumber;
     private String address;
-    private String gender;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
     private LocalDate birthOfDate;
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy = "userProfile")
+    private User user;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(String phoneNumber, String address, Gender gender, LocalDate birthOfDate) {
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.gender = gender;
+        this.birthOfDate = birthOfDate;
+    }
 }
