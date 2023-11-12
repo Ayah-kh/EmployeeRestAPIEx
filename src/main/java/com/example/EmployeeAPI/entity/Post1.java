e540f180e5edd36210ad350f8a4af68dccc29d3e
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-11-12 10:02:02 +0200

    First Commit

--- src/main/java/com/example/EmployeeAPI/entity/Post1.java
+++ src/main/java/com/example/EmployeeAPI/entity/Post1.java
@@ -1,4 +1,50 @@
 package com.example.EmployeeAPI.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
 public class Post1 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+
+    public Post1() {
+    }
+
+    public Post1(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    List<Comment> comments = new ArrayList<>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
